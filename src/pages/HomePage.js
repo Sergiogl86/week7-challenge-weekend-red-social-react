18c971ea83f589fabca964cb82a36933b0d2578c
Sergio Garcia <sergio.gl86@gmail.com>
2021-11-13 22:03:21 +0100

    Se completa el proceso de registro y login

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,4 @@
+import "./homePage.css";
 const HomePage = () => {
   return (
     <>
@@ -5,10 +6,13 @@ const HomePage = () => {
         <h1>Red social de Sergio!!</h1>
         <h2>Skylab Coders Academy</h2>
         <img
+          className="homepage-img"
           src="https://isdicoders.com/images/50912/default.png"
           alt="Skylab Coders Academy Logo"
         />
+
         <img
+          className="homepage-img"
           src="https://lh5.googleusercontent.com/p/AF1QipMYqFS3X-VcyLsFgXD1gKQudvziPUmac88lQyQ=w1080-k-no"
           alt="Skylab Coders Academy"
         />

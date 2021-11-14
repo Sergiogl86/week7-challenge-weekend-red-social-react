835d4f1b3334be4758868d7856bbf4972a1c2126
Sergio Garcia <sergio.gl86@gmail.com>
2021-11-14 17:30:45 +0100

    Se añade al test de integración de la App RegisterPage

--- src/App.test.js
+++ src/App.test.js
@@ -16,25 +16,46 @@ afterAll(() => {
 });
 
 describe("Given an App component", () => {
-  describe("When the user logged a user", () => {
-    test("Then it should show Hola username", async () => {
-      const loginNavbar = screen.getByTestId("navbar-login");
-      userEvent.click(loginNavbar);
+  describe("When the user register and logged a user and click on Social Network", () => {
+    test.only("Then it should show a list of members", async () => {
+      const registerNavbar = await screen.findByTestId("navbar-register");
+      userEvent.click(registerNavbar);
 
-      const username = screen.getByTestId("login-username");
-      const password = screen.getByTestId("login-password");
+      const registerUsername = await screen.findByTestId("register-username");
+      const registerName = await screen.findByTestId("register-name");
+      const registerPassword = await screen.findByTestId("register-password");
+      const registerAge = await screen.findByTestId("register-age");
+      const registerBio = await screen.findByTestId("register-bio");
+
+      userEvent.type(registerUsername, "Calimero");
+      userEvent.type(registerName, "Carlos Pajares");
+      userEvent.type(registerPassword, "entrar");
+      userEvent.type(registerAge, "30");
+      userEvent.type(
+        registerBio,
+        "Me gusta hacer rimas, soy el más grande, y pa cosas grandes aquí tengo mi fiambre"
+      );
+
+      const registrar = await screen.findByTestId("register-Registrar");
+      userEvent.click(registrar);
+
+      const username = await screen.findByTestId("login-username");
+      const password = await screen.findByTestId("login-password");
 
       userEvent.type(username, "Sergio_gl");
       userEvent.type(password, "entrar");
 
-      const entrar = screen.getByTestId("login-Entrar");
+      const entrar = await screen.findByTestId("login-Entrar");
       userEvent.click(entrar);
 
-      const greeting = await screen.findByText("Hola undefined");
+      const socialNavbar = await screen.findByTestId("navbar-social");
+      userEvent.click(socialNavbar);
+
+      const member = await screen.findByText("Calimero");
 
       screen.debug();
 
-      expect(greeting).toBeInTheDocument();
+      expect(member).toBeInTheDocument();
     });
   });
 });

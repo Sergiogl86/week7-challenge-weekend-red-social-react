377efdc9fe53c68ba87a5f3d75076bc54e6f1fc1
Sergio Garcia <sergio.gl86@gmail.com>
2021-11-16 08:43:23 +0100

    Se actualiza el perfil de usuario al cargar App

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,11 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
-import {
-  loginUserAction,
-  logOutUserAction,
-} from "../redux/actions/userActionCreators";
+import { logOutUserAction } from "../redux/actions/userActionCreators";
 import { loginUserThunk, registerUserThunk } from "../redux/thunks/userThunks";
-import jwtDecode from "jwt-decode";
 import { useCallback } from "react";
+import { checkTokenThunk } from "./../redux/thunks/membersThunks";
 
 const useUser = () => {
   const dispatch = useDispatch();
@@ -28,9 +25,7 @@ const useUser = () => {
 
   const checkToken = useCallback(() => {
     if (localStorage.getItem("userToken")) {
-      const token = localStorage.getItem("userToken");
-      const userData = jwtDecode(token);
-      dispatch(loginUserAction(userData));
+      dispatch(checkTokenThunk());
     }
   }, [dispatch]);
 
